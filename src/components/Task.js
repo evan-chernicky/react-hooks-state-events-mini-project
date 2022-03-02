aac124272feac725c317f8ed468f93556caaf852
Evan Chernicky <echernicky@gmail.com>
2022-03-02 11:00:31 -0500

    Started Project

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-function Task() {
+function Task({taskdata, onDeleteTask}) {
+
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div className="label">{taskdata.category}</div>
+      <div className="text">{taskdata.text}</div>
+      <button onClick={onDeleteTask} className="delete">X</button>
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
+import Task from './Task.js'
 
-function TaskList() {
+function TaskList({tasks}) {
+
+  const [taskList, setTaskList] = useState(tasks)
+
+  function onDeleteTask(e) {
+
+    console.log(e)
+
+  }
+
+  const renderTasks = tasks.map(task => <Task taskdata={task} onDeleteTask={onDeleteTask} key={task.text}/>  )
+
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {renderTasks}
     </div>
   );
 }
